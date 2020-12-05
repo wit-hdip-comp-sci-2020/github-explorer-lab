d2ddf365a94ee7c336d528dc65138702ff50e0fc
Eamonn de Leastar <edeleastar@wit.ie>
2020-12-05 06:22:10 +0000

    fetch repos from github api

--- explorer.js
+++ explorer.js
@@ -1,3 +1,8 @@
-function fetchRepos() {
-  console.log("Fetch Repos Called")
+async function fetchRepos() {
+  const githubId = document.getElementById("github-id").value;
+  const response = await fetch("https://api.github.com/users/" + githubId + "/repos");
+  if (response.status != 404) {
+    const repos = await response.json();
+    console.log(repos);
+  }
 }
