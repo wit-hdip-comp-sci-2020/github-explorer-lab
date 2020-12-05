5b0510b89d2bb05d3ac27b29513c933fee68aa9e
Eamonn de Leastar <edeleastar@wit.ie>
2020-12-05 06:41:56 +0000

    populate the table with repo names

--- explorer.js
+++ explorer.js
@@ -1,8 +1,22 @@
+function renderRepo(repo) {
+  const table = document.getElementById("repo-table");
+  const row = table.insertRow(-1);
+  const nameCell = row.insertCell(0);
+  nameCell.innerText = repo.name;
+}
+
+function renderAllRepos(repos) {
+  for (let i = 0; i < repos.length; i++) {
+    renderRepo(repos[i]);
+  }
+}
+
 async function fetchRepos() {
   const githubId = document.getElementById("github-id").value;
   const response = await fetch("https://api.github.com/users/" + githubId + "/repos");
   if (response.status != 404) {
     const repos = await response.json();
+    renderAllRepos(repos);
     console.log(repos);
   }
 }
